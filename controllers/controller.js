7bcef0153850a9f51f74e9ed52b178b78a1c5666
i'mfebri <iamfebriadji@gmail.com>
2021-02-27 22:29:01 +0700

    add update route

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,14 +5,16 @@ const
   db = require('../config/db')
 
 router.get('/', authenticateToken, (req, res, next) => {
-  const sql = 'SELECT * FROM ebooks'
-  db.query(sql, (err, result) => new Promise((resolve, reject) => {
-    if (err) return reject(err)
-    resolve(result)
+  db.query('SELECT * FROM ebooks', (err, result) => {
+    const promise = new Promise((resolve, reject) => {
+      if (err) return reject(err)
+      resolve(result)
+    })
+
+    promise
+      .then(result => res.json({ result }))
+      .catch(err => console.log(err))
   })
-  .then(result => res.json({ result }))
-  .catch(err => console.log(err))
-  )
 })
 
 router.post('/', authenticateToken, (req, res, next) => {
@@ -22,11 +24,32 @@ router.post('/', authenticateToken, (req, res, next) => {
   const sql = `INSERT INTO ebooks VALUE ( '${idEbooks}', '${title}', ${pages}, ${published}, null, null )`
   db.query(sql, err => new Promise((resolve, reject) => {
     if (err) return reject(err)
-    resolve('data berhasil ditambahkan')
+    resolve('data added successfully')
   })
-  .then(result => console.log(result))
+  .then(result => res.json({ result }))
   .catch(err => console.log(err))  
   )
+})
+
+router.put('/', authenticateToken, (req, res, next) => {
+  const 
+    { title, pages, published } = req.body,
+    queryStr = req.query.id,
+    idEbooks = randomres.str({ length: 12 })
+
+  if (!queryStr) return console.log('empty query')
+
+  const sql = `UPDATE ebooks SET id = ?, title = ?, pages = ?, published = ?, updated_at = null WHERE id = '${queryStr}'`
+  db.query(sql, [idEbooks, title, pages, published], err => {
+    const promise = new Promise((resolve, reject) => {
+      if (err) return reject(err)
+      resolve('data update successfully')
+    })
+
+    promise
+      .then(result => res.json({ result }))
+      .catch(err => console.log(err))
+  })
 })
 
 module.exports = router
